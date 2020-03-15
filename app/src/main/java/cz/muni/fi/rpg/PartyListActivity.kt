df5b14639698b349bd19f51132f7efe3ec9fba2f
František Maša <frantisekmasa1@gmail.com>
2020-03-15 17:21:46 +0100

    Add anonymous Firebase sign in to MainActivity

---
+++ app/src/main/java/cz/muni/fi/rpg/PartyListActivity.kt
@@ -0,0 +1,12 @@
+package cz.muni.fi.rpg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PartyListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_party_list)
+    }
+}
