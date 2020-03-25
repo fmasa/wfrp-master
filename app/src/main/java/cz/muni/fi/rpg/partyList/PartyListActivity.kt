3a2e8f5a0411303d47f19814babab7bfd8a8ee41
František Maša <frantisekmasa1@gmail.com>
2020-03-25 16:51:11 +0100

    Move PartyListActivity to partyList sub-package

--- app/src/main/java/cz/muni/fi/rpg/partyList/PartyListActivity.kt
+++ app/src/main/java/cz/muni/fi/rpg/partyList/PartyListActivity.kt
@@ -1,7 +1,8 @@
-package cz.muni.fi.rpg
+package cz.muni.fi.rpg.partyList
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cz.muni.fi.rpg.R
 
 class PartyListActivity : AppCompatActivity() {
 
