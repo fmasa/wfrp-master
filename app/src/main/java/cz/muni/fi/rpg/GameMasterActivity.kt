f372edd3875c845fea48c40a06e5b783408156cc
František Maša <frantisekmasa1@gmail.com>
2020-04-13 11:57:28 +0200

    Show invitation QR code and party name in game master activity

--- app/src/main/java/cz/muni/fi/rpg/GameMasterActivity.kt
+++ app/src/main/java/cz/muni/fi/rpg/GameMasterActivity.kt
@@ -1,22 +1,44 @@
 package cz.muni.fi.rpg
 
 import android.os.Bundle
+import com.google.gson.Gson
+import cz.muni.fi.rpg.model.PartyRepository
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_game_master.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.*
+import javax.inject.Inject
 
-class GameMasterActivity : DaggerAppCompatActivity() {
+class GameMasterActivity : DaggerAppCompatActivity(),
+    CoroutineScope by CoroutineScope(Dispatchers.Default) {
     companion object {
         const val EXTRA_PARTY_ID = "partyId"
     }
 
     private lateinit var partyId: UUID
 
+    @Inject
+    lateinit var parties: PartyRepository
+
+    @Inject
+    lateinit var gson: Gson
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         partyId = intent.getStringExtra(EXTRA_PARTY_ID)?.let { UUID.fromString(it) }
-            ?: throw IllegalAccessException("'${EXTRA_PARTY_ID}' must be provided");
+            ?: throw IllegalAccessException("'${EXTRA_PARTY_ID}' must be provided")
 
         setContentView(R.layout.activity_game_master)
+
+        launch {
+            val party = parties.get(partyId)
+
+            withContext(Dispatchers.Main) { supportActionBar?.title = party.name }
+            partyInviteQrCode.drawCode(gson.toJson(party.getInvitation()))
+        }
     }
 }
