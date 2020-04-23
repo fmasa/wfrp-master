9e6604f03c6e0f0ca3b157895b84349d597abf46
František Maša <frantisekmasa1@gmail.com>
2020-04-23 12:47:44 +0200

    Observe party for changes in GameMasterActivity

--- app/src/main/java/cz/muni/fi/rpg/ui/gameMaster/GameMasterActivity.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/gameMaster/GameMasterActivity.kt
@@ -1,30 +1,29 @@
 package cz.muni.fi.rpg.ui.gameMaster
 
 import android.os.Bundle
+import androidx.lifecycle.observe
 import com.google.gson.Gson
 import cz.muni.fi.rpg.R
-import cz.muni.fi.rpg.model.domain.party.PartyRepository
 import cz.muni.fi.rpg.ui.PartyScopedActivity
 import kotlinx.android.synthetic.main.activity_game_master.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
-class GameMasterActivity: PartyScopedActivity(R.layout.activity_game_master),
+class GameMasterActivity : PartyScopedActivity(R.layout.activity_game_master),
     CoroutineScope by CoroutineScope(Dispatchers.Default) {
     @Inject
     lateinit var gson: Gson
 
+    private val party by lazy { parties.getLive(getPartyId()) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        launch {
-            val party = parties.get(getPartyId())
-
-            withContext(Dispatchers.Main) { supportActionBar?.title = party.name }
-            partyInviteQrCode.drawCode(gson.toJson(party.getInvitation()))
+        party.observe(this) {
+            it.map { party ->
+                supportActionBar?.title = party.name
+                async { partyInviteQrCode.drawCode(gson.toJson(party.getInvitation())) }
+            }
         }
     }
 }
