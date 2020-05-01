59c033de7d0a2a5d57d9c7b75798a61d05f5a60d
František Maša <frantisekmasa1@gmail.com>
2020-05-01 21:35:37 +0200

    Added QueryLiveData class that abstracts transforming query result to LiveData

---
+++ app/src/main/java/cz/muni/fi/rpg/model/firestore/QueryLiveData.kt
@@ -0,0 +1,33 @@
+package cz.muni.fi.rpg.model.firestore
+
+import androidx.lifecycle.MutableLiveData
+import arrow.core.Left
+import arrow.core.Right
+import com.firebase.ui.firestore.SnapshotParser
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.Query
+
+class QueryLiveData<T>(
+    private val query: Query,
+    private val snapshotParser: SnapshotParser<T>
+) : MutableLiveData<List<T>>() {
+    private var listener: ListenerRegistration? = null
+
+    override fun onActive() {
+        super.onActive()
+
+        listener = query.addSnapshotListener { snapshot, exception ->
+            exception?.let { throw it }
+            snapshot?.let {
+                value = snapshot
+                    .asIterable()
+                    .map(snapshotParser::parseSnapshot)
+            }
+        }
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        listener?.remove()
+    }
+}
