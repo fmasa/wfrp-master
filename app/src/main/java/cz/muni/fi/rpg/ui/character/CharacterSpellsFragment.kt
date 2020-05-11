98d88ef5e385fdd3f0b1ac33fabdf452c8649dfa
František Maša <frantisekmasa1@gmail.com>
2020-05-11 11:17:52 +0200

    Added empty state UI for spells

--- app/src/main/java/cz/muni/fi/rpg/ui/character/CharacterSpellsFragment.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/character/CharacterSpellsFragment.kt
@@ -1,10 +1,18 @@
 package cz.muni.fi.rpg.ui.character
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import cz.muni.fi.rpg.R
 import cz.muni.fi.rpg.viewModels.CharacterViewModel
+import kotlinx.android.synthetic.main.fragment_character_spells.view.*
 
 class CharacterSpellsFragment : Fragment(R.layout.fragment_character_spells) {
     private val viewModel: CharacterViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.noSpellsIcon.visibility = View.VISIBLE;
+        view.noSpellsText.visibility = View.VISIBLE;
+    }
 }
