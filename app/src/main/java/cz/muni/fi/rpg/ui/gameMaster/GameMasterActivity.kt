22fba61a7281ed6d8ae4509d0b0850c3fff9fbfe
František Maša <frantisekmasa1@gmail.com>
2020-05-29 19:30:46 +0200

    Show character list in card and QR code in dialog

--- app/src/main/java/cz/muni/fi/rpg/ui/gameMaster/GameMasterActivity.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/gameMaster/GameMasterActivity.kt
@@ -3,17 +3,22 @@ package cz.muni.fi.rpg.ui.gameMaster
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.AttributeSet
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fasterxml.jackson.databind.json.JsonMapper
 import cz.muni.fi.rpg.R
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import cz.muni.fi.rpg.model.domain.character.CharacterId
 import cz.muni.fi.rpg.model.domain.character.CharacterRepository
+import cz.muni.fi.rpg.model.domain.party.Invitation
 import cz.muni.fi.rpg.model.right
 import cz.muni.fi.rpg.ui.PartyScopedActivity
 import cz.muni.fi.rpg.ui.character.CharacterActivity
 import cz.muni.fi.rpg.ui.gameMaster.adapter.CharacterAdapter
+import cz.muni.fi.rpg.ui.views.QrCode
 import kotlinx.android.synthetic.main.activity_game_master.*
 import kotlinx.coroutines.*
 import java.util.*
@@ -32,8 +37,11 @@ class GameMasterActivity : PartyScopedActivity(R.layout.activity_game_master),
 
     @Inject
     lateinit var jsonMapper: JsonMapper
+
     @Inject
     lateinit var characterRepo: CharacterRepository
+
+    private lateinit var invitation: Invitation
 
     private fun setViewVisibility(view: View, visible: Boolean) {
         view.visibility = if (visible) View.VISIBLE else View.GONE
@@ -50,8 +58,11 @@ class GameMasterActivity : PartyScopedActivity(R.layout.activity_game_master),
 
         partyViewModel.party.right().observe(this) { party ->
             supportActionBar?.title = party.name
-            launch { partyInviteQrCode.drawCode(jsonMapper.writeValueAsString(party.getInvitation())) }
+            inviteButton.isEnabled = true
+            invitation = party.getInvitation()
         }
+
+        inviteButton.setOnClickListener { showQrCode() }
 
         characterRepo.inParty(getPartyId()).observe(this) { characters ->
             if (characters.isNotEmpty()) {
@@ -67,5 +78,10 @@ class GameMasterActivity : PartyScopedActivity(R.layout.activity_game_master),
                 setEmptyCollectionView(true)
             }
         }
+    }
+
+    private fun showQrCode() {
+        QrCodeDialog(invitation, jsonMapper)
+            .show(supportFragmentManager, QrCodeDialog::class.simpleName)
     }
 }
