b5b1b038e07da48392b7f28ae51677092f8dd26b
František Maša <frantisekmasa1@gmail.com>
2020-06-05 22:08:54 +0200

    Make CharacterId parcelable

--- app/src/main/java/cz/muni/fi/rpg/model/domain/character/CharacterId.kt
+++ app/src/main/java/cz/muni/fi/rpg/model/domain/character/CharacterId.kt
@@ -1,5 +1,8 @@
 package cz.muni.fi.rpg.model.domain.character
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.util.UUID
 
-class CharacterId(val partyId: UUID, val userId: String)
+@Parcelize
+class CharacterId(val partyId: UUID, val userId: String) : Parcelable
