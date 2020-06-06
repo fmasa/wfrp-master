ce5a65e9454e3c5556d1585b6d57313671311748
František Maša <frantisekmasa1@gmail.com>
2020-06-06 16:06:27 +0200

    Show version in AboutFragment

--- app/src/main/java/cz/muni/fi/rpg/ui/common/AboutFragment.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/common/AboutFragment.kt
@@ -1,5 +1,15 @@
 package cz.muni.fi.rpg.ui.common
 
+import android.os.Bundle
+import android.view.View
+import cz.muni.fi.rpg.BuildConfig
 import cz.muni.fi.rpg.R
+import kotlinx.android.synthetic.main.fragment_about.view.*
 
-class AboutFragment : BaseFragment(R.layout.fragment_about)
+class AboutFragment : BaseFragment(R.layout.fragment_about) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.appVersion.text = BuildConfig.VERSION_NAME
+    }
+}
