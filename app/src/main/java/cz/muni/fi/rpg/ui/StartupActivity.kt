a10a3b816d1acb186abb53461d7c7e2d9f73b1df
František Maša <frantisekmasa1@gmail.com>
2020-06-18 22:44:27 +0200

    Extract authentication to view model

--- app/src/main/java/cz/muni/fi/rpg/ui/StartupActivity.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/StartupActivity.kt
@@ -1,40 +1,39 @@
 package cz.muni.fi.rpg.ui
 
 import android.content.Intent
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
 import cz.muni.fi.rpg.R
-import org.koin.android.ext.android.inject
+import cz.muni.fi.rpg.viewModels.AuthenticationViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.koin.android.viewmodel.ext.android.viewModel
 
-class StartupActivity : AppCompatActivity(R.layout.activity_startup) {
-    private val auth: FirebaseAuth by inject()
+class StartupActivity : AppCompatActivity(R.layout.activity_startup),
+    CoroutineScope by CoroutineScope(Dispatchers.Default) {
+
+    private val viewModel: AuthenticationViewModel by viewModel()
 
     override fun onStart() {
         super.onStart()
 
-        val currentUser = auth.currentUser
-
-        if (currentUser != null) {
-            showPartyList()
-            return
-        }
-
-        auth.signInAnonymously().addOnCompleteListener(this) { task ->
-            if (task.isSuccessful) {
-                Log.d(null, "User has signed in successfully")
-
+        launch {
+            if (viewModel.isAuthenticated() || viewModel.authenticateAnonymously()) {
                 showPartyList()
             } else {
-                Log.e(null, "Anonymous sign-in has failed", task.exception)
-                Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
 
-    private fun showPartyList() {
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+    private suspend fun showPartyList() {
+        withContext(Dispatchers.Main) {
+            startActivity(Intent(this@StartupActivity, MainActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/cz/muni/fi/rpg/viewModels/AuthenticationViewModel.kt
+++ app/src/main/java/cz/muni/fi/rpg/viewModels/AuthenticationViewModel.kt
@@ -1,8 +1,28 @@
 package cz.muni.fi.rpg.viewModels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.tasks.await
 
 class AuthenticationViewModel(private val auth: FirebaseAuth) : ViewModel() {
+
+    fun isAuthenticated() = auth.currentUser != null
+
+    /**
+     * @return true if user was successfully authenticated and false otherwise
+     */
+    suspend fun authenticateAnonymously(): Boolean {
+        return try {
+            auth.signInAnonymously().await()
+            Log.d(null, "User has signed in successfully")
+
+            true
+        } catch (e: Throwable) {
+            Log.e("AuthenticationViewModel", "Anonymous sign-in has failed", e)
+            false
+        }
+    }
+
     fun getUserId() = auth.currentUser?.uid ?: error("User is not authenticated")
 }
