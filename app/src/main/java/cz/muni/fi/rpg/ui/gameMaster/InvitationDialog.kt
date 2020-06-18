a541b9c00fa8ea16ab6c188e539f3deec17a4603
František Maša <frantisekmasa1@gmail.com>
2020-06-18 21:44:02 +0200

    Added button with invitation link to InvitationDialog

--- app/src/main/java/cz/muni/fi/rpg/ui/gameMaster/InvitationDialog.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/gameMaster/InvitationDialog.kt
@@ -1,16 +1,24 @@
 package cz.muni.fi.rpg.ui.gameMaster
 
 import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.fasterxml.jackson.databind.json.JsonMapper
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.dynamiclinks.ktx.shortLinkAsync
+import com.google.firebase.ktx.Firebase
 import cz.muni.fi.rpg.R
 import cz.muni.fi.rpg.model.domain.party.Invitation
 import kotlinx.android.synthetic.main.dialog_invitation.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class InvitationDialog(
@@ -26,9 +34,13 @@ class InvitationDialog(
             .create()
 
         val codeGenerating = launch {
-            view.partyInviteQrCode.drawCode(
-                withContext(Dispatchers.IO) { jsonMapper.writeValueAsString(invitation) }
-            )
+            val jsonInvitation = withContext(Dispatchers.IO) {
+                jsonMapper.writeValueAsString(invitation)
+            }
+
+            launch(Dispatchers.Main) { view.partyInviteQrCode.drawCode(jsonInvitation) }
+
+            initializeButton(view, invitation, jsonInvitation)
         }
 
         dialog.setOnDismissListener { codeGenerating.cancel() }
@@ -36,4 +48,32 @@ class InvitationDialog(
         return dialog
     }
 
+    private suspend fun initializeButton(
+        view: View,
+        invitation: Invitation,
+        jsonInvitation: String
+    ) {
+        val link = Firebase.dynamicLinks.shortLinkAsync {
+            link = Uri.parse("https://play.google.com/store/apps/details?id=cz.frantisekmasa.dnd&invitation=$jsonInvitation")
+            domainUriPrefix = "https://wfrp.page.link"
+        }.await()
+
+        Log.d(tag, "Invitation link was generated: ${link.shortLink}")
+
+        withContext(Dispatchers.Main) {
+            view.shareButton.setOnClickListener {
+                val sendIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(
+                        Intent.EXTRA_TEXT,
+                        "Join ${invitation.partyName} using this link: ${link.shortLink}"
+                    )
+                    type = "text/plain"
+                }
+
+                startActivity(Intent.createChooser(sendIntent, "Send link to your friends"))
+            }
+            view.shareButton.isEnabled = true
+        }
+    }
 }
