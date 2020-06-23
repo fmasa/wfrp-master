7852d04355cb6909424f2914de8a46d594bab77c
František Maša <frantisekmasa1@gmail.com>
2020-06-23 21:33:31 +0200

    Implement state saving for TextInput

--- app/src/main/java/cz/muni/fi/rpg/ui/views/TextInput.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/views/TextInput.kt
@@ -1,19 +1,29 @@
 package cz.muni.fi.rpg.ui.views
 
 import android.content.Context
+import android.os.Parcelable
 import android.text.InputFilter
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
 import androidx.core.content.res.getIntegerOrThrow
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputLayout
 import cz.muni.fi.rpg.R
+import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.view_text_input.view.*
 
 class TextInput(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+    @Parcelize
+    private data class SavedState(val value: String, val superState: Parcelable?) : Parcelable
+
+    private var value: String = ""
+
     private val textInputLayout: TextInputLayout
 
     init {
+        isSaveEnabled = true
+
         val view = inflate(context, R.layout.view_text_input, this)
         textInputLayout = view.textInputLayout
 
@@ -36,21 +46,39 @@ class TextInput(context: Context, attrs: AttributeSet) : LinearLayout(context, a
             )
         }
 
-        if (attributes.hasValue(R.styleable.TextInput_android_inputType)) {
-            textInputLayout.editText?.inputType =
-                attributes.getIntegerOrThrow(R.styleable.TextInput_android_inputType)
+        textInputLayout.editText?.let { editText ->
+            if (attributes.hasValue(R.styleable.TextInput_android_inputType)) {
+                editText.inputType = attributes.getIntegerOrThrow(R.styleable.TextInput_android_inputType)
+            }
+
+            editText.filters = filters.toTypedArray()
+            editText.setText(value)
+            editText.addTextChangedListener { value = it.toString().trim() }
         }
 
         attributes.recycle()
-
-        textInputLayout.editText?.filters = filters.toTypedArray()
     }
 
     fun getTextInputLayout(): TextInputLayout = textInputLayout
 
     fun setDefaultValue(value: String) {
-        textInputLayout.editText?.setText(value)
+        if (value == "") {
+            textInputLayout.editText?.setText(value)
+        }
     }
 
-    fun getValue(): String = requireNotNull(textInputLayout.editText).text.toString()
+    fun getValue(): String = value
+
+    override fun onSaveInstanceState(): Parcelable? {
+        return SavedState(value, super.onSaveInstanceState())
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        check(state is SavedState)
+
+        value = state.value
+        textInputLayout.editText?.setText(value)
+
+        super.onRestoreInstanceState(state.superState)
+    }
 }
