b6a71a27b83754ad5a3568c9552ed9fdf0522eb4
František Maša <frantisekmasa1@gmail.com>
2020-08-10 15:52:18 +0200

    Extract authentication to AuthenticationFragment

--- app/src/main/java/cz/muni/fi/rpg/ui/StartupActivity.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/StartupActivity.kt
@@ -1,102 +1,22 @@
 package cz.muni.fi.rpg.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.common.api.ApiException
+import androidx.fragment.app.commit
 import cz.muni.fi.rpg.R
-import cz.muni.fi.rpg.common.log.Reporter
-import cz.muni.fi.rpg.ui.common.toast
-import cz.muni.fi.rpg.viewModels.AuthenticationViewModel
-import kotlinx.coroutines.*
-import kotlinx.coroutines.tasks.await
-import org.koin.android.viewmodel.ext.android.viewModel
-import timber.log.Timber
-
+import cz.muni.fi.rpg.ui.common.AuthenticationFragment
 
 class StartupActivity : AppCompatActivity(R.layout.activity_startup),
-    CoroutineScope by CoroutineScope(Dispatchers.Default) {
-
-    companion object {
-        const val CODE_SIGN_IN = 1
-    }
-
-    private val viewModel: AuthenticationViewModel by viewModel()
+    AuthenticationFragment.Listener {
 
     override fun onStart() {
         super.onStart()
 
-        launch {
-            if (viewModel.isAuthenticated()) {
-                showPartyList()
-                return@launch
-            }
-
-            val context = applicationContext
-            val token = viewModel.obtainGoogleToken(context)
-
-            if (token == null) {
-                Timber.d("Could not obtain Google account, starting Sign-In")
-                withContext(Dispatchers.Main) {
-                    startActivityForResult(viewModel.getGoogleSignInIntent(context), CODE_SIGN_IN)
-                }
-                return@launch
-            }
-
-            authenticateWithGoogle(token)
-        }
+        supportFragmentManager.commit { add(AuthenticationFragment(), "Authentication") }
     }
 
-    private suspend fun authenticateWithGoogle(idToken: String) {
-        if (viewModel.connectGoogleToFirebaseAuth(idToken)) {
-            showPartyList()
-            return
-        }
-
-        withContext(Dispatchers.Main) {
-            fallbackToAnonymousAuthentication(null)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == CODE_SIGN_IN) {
-            launch {
-                try {
-                    val account = GoogleSignIn.getSignedInAccountFromIntent(data).await()
-                    authenticateWithGoogle(account.idToken ?: error("Token not set"))
-                } catch (e: ApiException) {
-                    Timber.e(e, "Google sign-in failed")
-                    fallbackToAnonymousAuthentication(e.message)
-                } catch (e : Throwable) {
-                    Timber.e(e)
-                    throw e
-                }
-            }
-        }
-    }
-
-    private fun fallbackToAnonymousAuthentication(message: String?) {
-        AlertDialog.Builder(this)
-            .setMessage(message ?: getString(R.string.google_sign_in_error))
-            .setNeutralButton(android.R.string.ok) { _, _ ->
-                launch {
-                    if (viewModel.authenticateAnonymously()) {
-                        showPartyList()
-                    } else {
-                        withContext(Dispatchers.Main) { toast("Authentication failed.") }
-                    }
-                }
-            }.show()
-    }
-
-    private suspend fun showPartyList() {
-        withContext(Dispatchers.Main) {
-            Reporter.setUserId(viewModel.getUserId())
-            startActivity(Intent(this@StartupActivity, MainActivity::class.java))
-            finish()
-        }
+    override fun onAuthenticated() {
+        startActivity(Intent(this@StartupActivity, MainActivity::class.java))
+        finish()
     }
 }
---
+++ app/src/main/java/cz/muni/fi/rpg/ui/common/AuthenticationFragment.kt
@@ -0,0 +1,121 @@
+package cz.muni.fi.rpg.ui.common
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
+import cz.muni.fi.rpg.R
+import cz.muni.fi.rpg.common.log.Reporter
+import cz.muni.fi.rpg.viewModels.AuthenticationViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import org.koin.android.viewmodel.ext.android.viewModel
+import timber.log.Timber
+
+/**
+ * This fragment takes care of all authentication methods available to user
+ * 
+ * Every entrypoint to application should implement AuthenticationFragment.Listener and add instance
+ * of this fragment to FragmentManager
+ *
+ * @see AuthenticationFragment.Listener
+ */
+class AuthenticationFragment : Fragment(), CoroutineScope by CoroutineScope(Dispatchers.Default) {
+
+    companion object {
+        const val CODE_SIGN_IN = 1
+    }
+
+    interface Listener {
+        fun onAuthenticated()
+    }
+
+    private val viewModel: AuthenticationViewModel by viewModel()
+    private lateinit var listener: Listener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        require(context is Listener)
+        listener = context
+    }
+
+    private suspend fun authenticateWithGoogle(idToken: String) {
+        if (viewModel.connectGoogleToFirebaseAuth(idToken)) {
+            notifyDependentsThatUserIsAuthenticated()
+            return
+        }
+
+        withContext(Dispatchers.Main) {
+            fallbackToAnonymousAuthentication(null)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        launch {
+            if (viewModel.isAuthenticated()) {
+                notifyDependentsThatUserIsAuthenticated()
+                return@launch
+            }
+
+            val context = requireContext()
+
+            viewModel.obtainGoogleToken(context)?.let {
+                authenticateWithGoogle(it)
+                return@launch
+            }
+
+            Timber.d("Could not obtain Google account, starting Sign-In")
+            withContext(Dispatchers.Main) {
+                startActivityForResult(viewModel.getGoogleSignInIntent(context), CODE_SIGN_IN)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CODE_SIGN_IN) {
+            launch {
+                try {
+                    val account = GoogleSignIn.getSignedInAccountFromIntent(data).await()
+                    authenticateWithGoogle(account.idToken ?: error("Token not set"))
+                } catch (e: ApiException) {
+                    Timber.e(e, "Google sign-in failed")
+                    fallbackToAnonymousAuthentication(e.message)
+                } catch (e: Throwable) {
+                    Timber.e(e)
+                    throw e
+                }
+            }
+        }
+    }
+
+    private fun fallbackToAnonymousAuthentication(message: String?) {
+        AlertDialog.Builder(requireContext())
+            .setMessage(message ?: getString(R.string.google_sign_in_error))
+            .setNeutralButton(android.R.string.ok) { _, _ ->
+                launch {
+                    if (viewModel.authenticateAnonymously()) {
+                        notifyDependentsThatUserIsAuthenticated()
+                    } else {
+                        withContext(Dispatchers.Main) { toast("Authentication failed.") }
+                    }
+                }
+            }.show()
+    }
+
+    private suspend fun notifyDependentsThatUserIsAuthenticated() {
+        Reporter.setUserId(viewModel.getUserId())
+
+        withContext(Dispatchers.Main) { listener.onAuthenticated() }
+    }
+}
