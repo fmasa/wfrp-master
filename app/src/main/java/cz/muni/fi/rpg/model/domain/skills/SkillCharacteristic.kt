6635f9b372c27d1977482a59a8586abf8f0a9da2
František Maša <frantisekmasa1@gmail.com>
2020-08-16 17:20:14 +0200

    Added WS, BS and Initative as possible skill characteristics

--- app/src/main/java/cz/muni/fi/rpg/model/domain/skills/SkillCharacteristic.kt
+++ app/src/main/java/cz/muni/fi/rpg/model/domain/skills/SkillCharacteristic.kt
@@ -5,22 +5,28 @@ import cz.muni.fi.rpg.R
 
 enum class SkillCharacteristic {
     AGILITY,
+    BALLISTIC_SKILL,
     DEXTERITY,
+    INITIATIVE,
     INTELLIGENCE,
     FELLOWSHIP,
     STRENGTH,
     TOUGHNESS,
+    WEAPON_SKILL,
     WILL_POWER;
 
     @StringRes
     fun getReadableNameId(): Int {
         return when (this) {
             AGILITY -> R.string.label_agility
+            BALLISTIC_SKILL -> R.string.label_ballistic_skill
             DEXTERITY -> R.string.label_dexterity
+            INITIATIVE -> R.string.label_initiative
             INTELLIGENCE -> R.string.label_intelligence
             FELLOWSHIP -> R.string.label_fellowship
             STRENGTH -> R.string.label_strength
             TOUGHNESS -> R.string.label_toughness
+            WEAPON_SKILL -> R.string.label_weapon_skill
             WILL_POWER -> R.string.label_will_power
         }
     }
