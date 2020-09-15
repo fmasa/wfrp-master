ba63006b21dc60ac18c0bb732d78a9a6128102c4
František Maša <frantisekmasa1@gmail.com>
2020-09-15 20:56:24 +0200

    Reimplement FAB using Jetpack Compose

---
+++ app/src/main/java/cz/muni/fi/rpg/ui/common/composables/FloatingActionsMenu.kt
@@ -0,0 +1,93 @@
+package cz.muni.fi.rpg.ui.common.composables
+
+import androidx.compose.animation.DpPropKey
+import androidx.compose.animation.core.*
+import androidx.compose.animation.transition
+import androidx.compose.foundation.Icon
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawOpacity
+import androidx.compose.ui.drawLayer
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import cz.muni.fi.rpg.R
+
+enum class MenuState {
+    COLLAPSED,
+    EXPANDED,
+}
+
+private fun toState(state: MenuState) = when (state) {
+    MenuState.COLLAPSED -> MenuState.EXPANDED
+    MenuState.EXPANDED -> MenuState.COLLAPSED
+}
+
+private val opacity = FloatPropKey()
+private val yOffset = DpPropKey()
+private val iconRotation = FloatPropKey()
+
+private const val animationLengthMillis = 200
+
+private val transitionDefinition = transitionDefinition<MenuState> {
+    //2
+    state(MenuState.COLLAPSED) {
+        this[opacity] = 0f
+        this[yOffset] = 0.dp
+        this[iconRotation] = 0f
+    }
+    //3
+    state(MenuState.EXPANDED) {
+        this[opacity] = 1f
+        this[yOffset] = 20.dp
+        this[iconRotation] = 90f + 45f
+    }
+
+    //4
+    transition(MenuState.COLLAPSED to MenuState.EXPANDED) {
+        opacity using tween(durationMillis = animationLengthMillis)
+        yOffset using spring(Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessHigh)
+        iconRotation using tween(durationMillis = animationLengthMillis)
+    }
+
+    //5
+    transition(MenuState.EXPANDED to MenuState.COLLAPSED) {
+        opacity using tween(durationMillis = animationLengthMillis)
+        yOffset using tween(durationMillis = animationLengthMillis)
+        iconRotation using tween(durationMillis = animationLengthMillis)
+    }
+}
+
+@Composable
+fun FloatingActionsMenu(
+    state: MutableState<MenuState>,
+    subButtons: @Composable ColumnScope.() -> Unit
+) {
+    val transition = transition(definition = transitionDefinition, toState = state.value)
+
+    Column(horizontalGravity = Alignment.End) {
+        Column(
+            horizontalGravity = Alignment.End,
+            verticalArrangement = Arrangement.spacedBy(transition[yOffset]),
+            modifier = Modifier
+                .drawOpacity(transition[opacity])
+                .padding(bottom = transition[yOffset])
+        ) {
+            if (state.value == MenuState.EXPANDED) {
+                subButtons()
+            }
+        }
+
+        FloatingActionButton(
+            onClick = { state.value = toState(state.value) },
+        ) {
+            Icon(
+                vectorResource(R.drawable.ic_add),
+                modifier = Modifier.drawLayer(rotationZ = transition[iconRotation])
+            )
+        }
+    }
+}
--- app/src/main/java/cz/muni/fi/rpg/ui/partyList/PartyListFragment.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/partyList/PartyListFragment.kt
@@ -1,22 +1,25 @@
 package cz.muni.fi.rpg.ui.partyList
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Divider
+import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.ListItem
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.fragment.findNavController
-import com.getbase.floatingactionbutton.FloatingActionsMenu
 import cz.muni.fi.rpg.R
 import cz.muni.fi.rpg.model.domain.character.CharacterId
 import cz.muni.fi.rpg.model.domain.party.Party
 import cz.muni.fi.rpg.ui.common.BaseFragment
 import cz.muni.fi.rpg.ui.common.toast
-import cz.muni.fi.rpg.ui.joinParty.JoinPartyActivity
 import cz.muni.fi.rpg.viewModels.AuthenticationViewModel
 import cz.muni.fi.rpg.viewModels.PartyListViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -33,47 +36,70 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.gesture.longPressGestureFilter
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import cz.muni.fi.rpg.ui.common.composables.*
+import cz.muni.fi.rpg.ui.joinParty.JoinPartyActivity
 
-class PartyListFragment : BaseFragment(R.layout.fragment_party_list),
+class PartyListFragment : BaseFragment(0),
     AssemblePartyDialog.PartyCreationListener,
     CoroutineScope by CoroutineScope(Dispatchers.Default) {
 
     private val viewModel: PartyListViewModel by viewModel()
     private val authViewModel: AuthenticationViewModel by sharedViewModel()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = ComposeView(requireContext()).apply {
         val userId = authViewModel.getUserId()
 
-        view.findViewById<ComposeView>(R.id.compose).setContent {
+        setContent {
             Theme {
-                MainContainer(
-                    userId,
-                    viewModel,
-                    onClick = {
-                        if (it.gameMasterId == authViewModel.getUserId()) {
-                            openGameMasterFragment(it.id)
-                        } else {
-                            openCharacter(it.id, userId)
+                val menuState = remember { mutableStateOf(MenuState.COLLAPSED) }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxHeight(),
+                    floatingActionButton = {
+                        FloatingActionsMenu(menuState) {
+                            ExtendedFloatingActionButton(
+                                icon = { Icon(vectorResource(R.drawable.ic_camera)) },
+                                text = { Text(stringResource(R.string.scanCode_title)) },
+                                onClick = {
+                                    JoinPartyActivity.start(requireContext())
+                                    menuState.value = MenuState.COLLAPSED
+                                }
+                            )
+                            ExtendedFloatingActionButton(
+                                icon = { Icon(vectorResource(R.drawable.ic_group_add)) },
+                                text = { Text(stringResource(R.string.assembleParty_title)) },
+                                onClick = {
+                                    AssemblePartyDialog().show(childFragmentManager, null)
+                                    menuState.value = MenuState.COLLAPSED
+                                }
+                            )
                         }
-                    },
-                    onRemove = ::removeParty,
-                )
+                    }
+                ) {
+                    MainContainer(
+                        Modifier.clickable(
+                            onClick = { menuState.value = MenuState.COLLAPSED },
+                            indication = null,
+                        ),
+                        userId,
+                        viewModel,
+                        onClick = {
+                            if (it.gameMasterId == authViewModel.getUserId()) {
+                                openGameMasterFragment(it.id)
+                            } else {
+                                openCharacter(it.id, userId)
+                            }
+                        },
+                        onRemove = ::removeParty,
+                    )
+                }
             }
-        }
-
-        val fabMenu = view.findViewById<FloatingActionsMenu>(R.id.fabMenu)
-        view.findViewById<View>(R.id.assembleNewParty).setOnClickListener {
-            AssemblePartyDialog().show(childFragmentManager, "AssemblePartyDialog")
-            fabMenu.collapse()
-        }
-
-        view.findViewById<View>(R.id.scanQrCode).setOnClickListener {
-            JoinPartyActivity.start(requireContext())
-            fabMenu.collapse()
         }
     }
 
@@ -134,6 +160,7 @@ fun PartyItem(party: Party, onClick: () -> Unit, onLongPress: () -> Unit) {
 
 @Composable
 private fun MainContainer(
+    modifier: Modifier,
     userId: String,
     viewModel: PartyListViewModel,
     onClick: (Party) -> Unit,
@@ -141,19 +168,21 @@ private fun MainContainer(
 ) {
     val parties = viewModel.liveForUser(userId).observeAsState()
 
-    parties.value?.let {
-        if (it.isEmpty()) {
-            EmptyUI(R.string.no_parties_prompt, R.drawable.ic_rally_the_troops)
-            return
-        }
+    Box(modifier) {
+        parties.value?.let {
+            if (it.isEmpty()) {
+                EmptyUI(R.string.no_parties_prompt, R.drawable.ic_rally_the_troops)
+                return@Box
+            }
 
-        PartyList(parties = it, onClick = onClick, onRemove = onRemove)
+            PartyList(parties = it, onClick = onClick, onRemove = onRemove)
+        }
     }
 }
 
 @Composable
 fun PartyList(parties: List<Party>, onClick: (Party) -> Unit, onRemove: (Party) -> Unit) {
-    ScrollableColumn(Modifier.padding(top = 12.dp)) {
+    ScrollableColumn(Modifier.padding(top = 12.dp).fillMaxHeight()) {
         val contextMenuOpened = remember { mutableStateOf<UUID?>(null) }
 
         for (party in parties) {
