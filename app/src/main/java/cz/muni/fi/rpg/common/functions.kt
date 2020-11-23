68ac90e339d9b1356fb10513f910db53a78b0563
František Maša <frantisekmasa1@gmail.com>
2020-11-23 16:39:02 +0100

    Added helper method for max length validation

--- app/src/main/java/cz/muni/fi/rpg/common/functions.kt
+++ app/src/main/java/cz/muni/fi/rpg/common/functions.kt
@@ -2,3 +2,8 @@ package cz.muni.fi.rpg.common
 
 typealias EntityListener<TEntity> = (item: TEntity) -> Unit
 typealias SuspendableEntityListener<TEntity> = suspend (item: TEntity) -> Unit
+
+fun String.requireMaxLength(maxLength: Int, valueName: String)
+    = require(length <= maxLength) {
+    "Maximum allowed length of \"$valueName\" is $maxLength, \"$this\" given"
+}
