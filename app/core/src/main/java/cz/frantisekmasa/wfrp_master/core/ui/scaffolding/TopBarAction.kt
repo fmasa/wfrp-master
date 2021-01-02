5979db1915bd9a2115aefcdc2f4a09e8e75e0690
František Maša <frantisekmasa1@gmail.com>
2021-01-02 16:34:24 +0100

    Added TopBarAction with easier API

--- app/core/src/main/java/cz/frantisekmasa/wfrp_master/core/ui/scaffolding/TopBarAction.kt
+++ app/core/src/main/java/cz/frantisekmasa/wfrp_master/core/ui/scaffolding/TopBarAction.kt
@@ -1,10 +1,15 @@
 package cz.frantisekmasa.wfrp_master.core.ui.scaffolding
 
+import androidx.annotation.StringRes
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Providers
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
 
+@Deprecated("Use version that expects only string resource")
 @Composable
 fun TopBarAction(
     modifier: Modifier = Modifier,
@@ -17,5 +22,21 @@ fun TopBarAction(
 
     TextButton(onClick = onClick, enabled = enabled, modifier = modifier) {
         Providers(AmbientContentColor provides contentColor) { content() }
+    }
+}
+
+@Composable
+fun TopBarAction(
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+    @StringRes textRes: Int,
+) {
+    TopBarAction(
+        modifier = modifier,
+        enabled = enabled,
+        onClick = onClick
+    ) {
+        Text(stringResource(textRes).toUpperCase(Locale.current))
     }
 }
