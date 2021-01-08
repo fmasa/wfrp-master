0e42ad834b63deab1c82cebadd57fb47ed976dfa
František Maša <frantisekmasa1@gmail.com>
2021-01-08 01:58:08 +0100

    Use IconButtons for increment/decrement buttons in NumberPicker

--- app/core/src/main/java/cz/frantisekmasa/wfrp_master/core/ui/primitives/NumberPicker.kt
+++ app/core/src/main/java/cz/frantisekmasa/wfrp_master/core/ui/primitives/NumberPicker.kt
@@ -1,19 +1,11 @@
 package cz.frantisekmasa.wfrp_master.core.ui.primitives
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import cz.frantisekmasa.wfrp_master.core.R
@@ -29,7 +21,9 @@ fun NumberPicker(
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.padding(vertical = 4.dp).then(modifier)
+        modifier = modifier
+            .padding(vertical = 4.dp)
+            .then(modifier)
     ) {
         if (label != null) {
             Text(label, style = MaterialTheme.typography.subtitle1)
@@ -39,27 +33,27 @@ fun NumberPicker(
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            val iconColorFilter =
-                ColorFilter.tint(MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium))
+            val tint = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
 
-            Image(
-                modifier = Modifier.clickable(onClick = onDecrement),
-                imageVector = vectorResource(R.drawable.ic_remove_circle),
-                colorFilter = iconColorFilter,
-            )
+            IconButton(
+                onClick = onDecrement,
+                modifier = Modifier.preferredSize(width = 48.dp, height = 24.dp),
+            ) {
+                Icon(vectorResource(R.drawable.ic_remove_circle), tint = tint)
+            }
 
             Text(
                 value.toString(),
                 style = MaterialTheme.typography.h5,
                 color = color,
-                modifier = Modifier.padding(horizontal = 12.dp)
             )
 
-            Image(
-                modifier = Modifier.clickable(onClick = onIncrement),
-                imageVector = vectorResource(R.drawable.ic_add_circle),
-                colorFilter = iconColorFilter,
-            )
+            IconButton(
+                onClick = onIncrement,
+                modifier = Modifier.preferredSize(width = 48.dp, height = 24.dp),
+            ) {
+                Icon(vectorResource(R.drawable.ic_add_circle), tint = tint)
+            }
         }
     }
 }
