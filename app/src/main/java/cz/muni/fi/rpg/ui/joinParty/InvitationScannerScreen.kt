181b38aa74f1d872404a0ae017356891c8394ef6
František Maša <frantisekmasa1@gmail.com>
2021-01-17 20:57:26 +0100

    Reimplement InvitationLinkActivity using Jetpack Compose

--- app/src/main/java/cz/muni/fi/rpg/ui/joinParty/InvitationScannerScreen.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/joinParty/InvitationScannerScreen.kt
@@ -1,47 +1,30 @@
 package cz.muni.fi.rpg.ui.joinParty
 
 import android.Manifest
-import android.content.Context
 import android.os.Parcelable
-import android.text.SpannableStringBuilder
 import android.widget.Toast
-import androidx.annotation.MainThread
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.savedinstancestate.savedInstanceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.AmbientContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.core.text.bold
 import com.eazypermissions.common.model.PermissionResult
 import com.eazypermissions.coroutinespermission.PermissionManager
-import cz.frantisekmasa.wfrp_master.core.auth.AmbientUser
 import cz.frantisekmasa.wfrp_master.core.ui.buttons.BackButton
-import cz.frantisekmasa.wfrp_master.core.ui.primitives.FullScreenProgress
 import cz.frantisekmasa.wfrp_master.core.ui.scaffolding.SubheadBar
 import cz.frantisekmasa.wfrp_master.core.ui.viewinterop.activity
 import cz.frantisekmasa.wfrp_master.navigation.Route
 import cz.frantisekmasa.wfrp_master.navigation.Routing
 import cz.muni.fi.rpg.R
-import cz.muni.fi.rpg.model.domain.invitation.AlreadyInParty
-import cz.muni.fi.rpg.model.domain.invitation.InvalidInvitation
 import cz.frantisekmasa.wfrp_master.core.domain.party.Invitation
-import cz.muni.fi.rpg.ui.common.composables.PrimaryButton
 import cz.muni.fi.rpg.ui.common.toast
 import cz.muni.fi.rpg.viewModels.provideJoinPartyViewModel
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import kotlinx.parcelize.Parcelize
-import timber.log.Timber
 
 @Composable
 fun InvitationScannerScreen(routing: Routing<Route>) {
@@ -112,62 +95,16 @@ fun InvitationScannerScreen(routing: Routing<Route>) {
                         },
                     )
                 }
-                InvitationScannerScreenState.ProcessingInvitation -> FullScreenProgress()
                 is InvitationScannerScreenState.WaitingForUserConfirmation -> {
-                    Text(
-                        text = SpannableStringBuilder()
-                            .append(stringResource(R.string.join_party_dialog_party_name))
-                            .append(" ")
-                            .bold { append(state.invitation.partyName) }
-                            .append(".")
-                            .toString(),
-                        style = MaterialTheme.typography.h5,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-
-                    val userId = AmbientUser.current.id
-                    val context = AmbientContext.current
-
-                    PrimaryButton(
-                        R.string.title_joinParty,
-                        onClick = {
-                            val invitation = state.invitation
-                            screenState = InvitationScannerScreenState.ProcessingInvitation
-
-                            coroutineScope.launch {
-                                try {
-                                    withContext(Dispatchers.IO) {
-                                        viewModel.acceptInvitation(userId, invitation)
-                                    }
-
-                                    withContext(Dispatchers.Main) {
-                                        routing.pop()
-                                    }
-                                } catch (e: Throwable) {
-                                    when (e) {
-                                        is InvalidInvitation -> {
-                                            showError(e, context, R.string.error_invalid_invitation)
-                                        }
-                                        is AlreadyInParty -> {
-                                            showError(
-                                                e,
-                                                context,
-                                                R.string.error_already_party_member
-                                            )
-                                        }
-                                        is CancellationException -> {
-                                        }
-                                        else -> {
-                                            showError(e, context, R.string.error_unkown)
-                                            Timber.e(e)
-                                        }
-                                    }
-
-                                    screenState = InvitationScannerScreenState.Scanning
-                                }
-                            }
-                        }
+                    InvitationConfirmation(
+                        state.invitation,
+                        viewModel,
+                        onSuccess = {
+                            routing.pop()
+                        },
+                        onError = {
+                            screenState = InvitationScannerScreenState.Scanning
+                        },
                     )
                 }
             }
@@ -183,20 +120,7 @@ private sealed class InvitationScannerScreenState : Parcelable {
     object Scanning : InvitationScannerScreenState()
 
     @Parcelize
-    object ProcessingInvitation : InvitationScannerScreenState()
-
-    @Parcelize
     data class WaitingForUserConfirmation(val invitation: Invitation) : InvitationScannerScreenState()
-}
-
-@MainThread
-private fun showError(
-    e: Throwable,
-    context: Context,
-    @StringRes messageRes: Int,
-) {
-    Toast.makeText(context, context.getString(messageRes), Toast.LENGTH_SHORT).show()
-    Timber.i(e, "Invitation processing error: ${e.message}")
 }
 
 private const val PermissionRequestCode = 10
