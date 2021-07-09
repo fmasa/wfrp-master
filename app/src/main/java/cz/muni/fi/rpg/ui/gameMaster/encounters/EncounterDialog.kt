ff2c1d2e7094a0f1751dd2d001d7db05e80120bb
František Maša <frantisekmasa1@gmail.com>
2021-07-10 01:28:55 +0200

    Use FormDialog in EncounterDialog

--- app/src/main/java/cz/muni/fi/rpg/ui/gameMaster/encounters/EncounterDialog.kt
+++ app/src/main/java/cz/muni/fi/rpg/ui/gameMaster/encounters/EncounterDialog.kt
@@ -1,36 +1,19 @@
 package cz.muni.fi.rpg.ui.gameMaster.encounters
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import cz.frantisekmasa.wfrp_master.combat.domain.encounter.Encounter
 import cz.frantisekmasa.wfrp_master.core.domain.party.PartyId
-import cz.frantisekmasa.wfrp_master.core.ui.buttons.CloseButton
+import cz.frantisekmasa.wfrp_master.core.ui.components.FormDialog
 import cz.frantisekmasa.wfrp_master.core.ui.dialogs.FullScreenDialog
+import cz.frantisekmasa.wfrp_master.core.ui.forms.HydratedFormData
+import cz.frantisekmasa.wfrp_master.core.ui.forms.InputValue
 import cz.frantisekmasa.wfrp_master.core.ui.forms.Rules
 import cz.frantisekmasa.wfrp_master.core.ui.forms.TextInput
 import cz.frantisekmasa.wfrp_master.core.ui.forms.inputValue
-import cz.frantisekmasa.wfrp_master.core.ui.primitives.Spacing
-import cz.frantisekmasa.wfrp_master.core.ui.scaffolding.SaveAction
 import cz.frantisekmasa.wfrp_master.core.viewModel.viewModel
 import cz.muni.fi.rpg.R
 import cz.muni.fi.rpg.viewModels.EncountersViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.koin.core.parameter.parametersOf
 
 @Composable
@@ -40,82 +23,61 @@ fun EncounterDialog(
     onDismissRequest: () -> Unit,
 ) {
     val viewModel: EncountersViewModel by viewModel { parametersOf(partyId) }
-    var validate by remember { mutableStateOf(false) }
-
-    val name = inputValue(existingEncounter?.name ?: "", Rules.NotBlank())
-    val description = inputValue(existingEncounter?.description ?: "")
 
     FullScreenDialog(onDismissRequest = onDismissRequest) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    navigationIcon = { CloseButton(onClick = onDismissRequest) },
-                    title = {
-                        Text(
-                            stringResource(
-                                when {
-                                    existingEncounter != null -> R.string.title_encounter_edit
-                                    else -> R.string.title_encounter_create
-                                }
-                            )
-                        )
-                    },
-                    actions = {
-                        val coroutineScope = rememberCoroutineScope()
-                        var saving by remember { mutableStateOf(false) }
+        val formData = EncounterDialogFormData.fromEncounter(existingEncounter)
 
-                        SaveAction(
-                            enabled = !saving,
-                            onClick = {
-                                if (!name.isValid() || !description.isValid()) {
-                                    validate = true
-                                    return@SaveAction
-                                }
-
-                                saving = true
-
-                                coroutineScope.launch(Dispatchers.IO) {
-                                    val encounterId = existingEncounter?.id
-
-                                    if (encounterId == null) {
-                                        viewModel.createEncounter(name.value, description.value)
-                                    } else {
-                                        viewModel.updateEncounter(
-                                            encounterId,
-                                            name.value,
-                                            description.value,
-                                        )
-                                    }
-
-                                    withContext(Dispatchers.Main) { onDismissRequest() }
-                                }
-                            }
-                        )
-                    }
-                )
+        FormDialog(
+            title = if (existingEncounter != null)
+                R.string.title_encounter_edit
+            else R.string.title_encounter_create,
+            onDismissRequest = onDismissRequest,
+            formData = formData,
+            onSave = {
+                if (existingEncounter != null) {
+                    viewModel.updateEncounter(existingEncounter.id, it.name, it.description)
+                } else {
+                    viewModel.createEncounter(it.name, it.description)
+                }
             }
-        ) {
-            Column(
-                Modifier
-                    .verticalScroll(rememberScrollState())
-                    .padding(Spacing.bodyPadding),
-                verticalArrangement = Arrangement.spacedBy(Spacing.small),
-            ) {
-                TextInput(
-                    label = stringResource(R.string.label_name),
-                    value = name,
-                    validate = validate,
-                    maxLength = Encounter.NAME_MAX_LENGTH,
-                )
+        ) { validate ->
+            TextInput(
+                label = stringResource(R.string.label_name),
+                value = formData.name,
+                validate = validate,
+                maxLength = Encounter.NAME_MAX_LENGTH,
+            )
 
-                TextInput(
-                    label = stringResource(R.string.label_description),
-                    value = description,
-                    validate = validate,
-                    maxLength = Encounter.DESCRIPTION_MAX_LENGTH,
-                    multiLine = true,
-                )
-            }
+            TextInput(
+                label = stringResource(R.string.label_description),
+                value = formData.description,
+                validate = validate,
+                maxLength = Encounter.DESCRIPTION_MAX_LENGTH,
+                multiLine = true,
+            )
         }
+    }
+}
+
+private class EncounterDialogFormData(
+    val name: InputValue,
+    val description: InputValue,
+) : HydratedFormData<EncounterDialogFormData.Value> {
+
+    data class Value(
+        val name: String,
+        val description: String,
+    )
+
+    override fun isValid(): Boolean = name.isValid() && description.isValid()
+
+    override fun toValue(): Value = Value(name = name.value, description = description.value)
+
+    companion object {
+        @Composable
+        fun fromEncounter(encounter: Encounter?) = EncounterDialogFormData(
+            name = inputValue(encounter?.name ?: "", Rules.NotBlank()),
+            description = inputValue(encounter?.description ?: ""),
+        )
     }
 }
