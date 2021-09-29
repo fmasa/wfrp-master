23023cff0e02b23504c29338dbb105d1cad285f0
František Maša <frantisekmasa1@gmail.com>
2021-09-29 21:17:03 +0200

    Use KTS for settings.gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name="rpg"
+include(":app")
+include(":app:core")
+include(":app:compendium")
+include(":app:navigation")
+include(":app:combat")
+include(":app:inventory")
+include(":app:religion")
