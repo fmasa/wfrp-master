a473d3e2e2c110e61ac77fbcac4ad258a8ba573f
František Maša <frantisekmasa1@gmail.com>
2022-01-23 13:34:27 +0100

    Add way to determine production/debug versions in composition

---
+++ common/src/commonMain/kotlin/cz/frantisekmasa/wfrp_master/common/core/LocalStaticConfiguration.kt
@@ -0,0 +1,9 @@
+package cz.frantisekmasa.wfrp_master.common.core
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import cz.frantisekmasa.wfrp_master.common.core.config.StaticConfiguration
+
+
+val LocalStaticConfiguration = staticCompositionLocalOf<StaticConfiguration> {
+    error("Static configuration was not provided")
+}
