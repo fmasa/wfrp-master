05bcbc9488f222a1a611cdc0d46745c803b00260
František Maša <frantisekmasa1@gmail.com>
2022-02-09 19:49:21 +0100

    Add Dispatchers.IO for common module

---
+++ common/src/commonMain/kotlin/cz/frantisekmasa/wfrp_master/common/core/shared/Dispatchers.kt
@@ -0,0 +1,6 @@
+package cz.frantisekmasa.wfrp_master.common.core.shared
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+expect val Dispatchers.IO: CoroutineDispatcher
