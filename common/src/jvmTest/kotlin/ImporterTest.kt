964c95368e5e247f8dab3c7061cf2ed2b3d9086a
František Maša <frantisekmasa1@gmail.com>
2022-12-27 01:13:29 +0100

    Test number of imported talents

--- common/src/jvmTest/kotlin/ImporterTest.kt
+++ common/src/jvmTest/kotlin/ImporterTest.kt
@@ -1,17 +1,33 @@
 
 import com.lowagie.text.pdf.PdfReader
 import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.RulebookCareerImporter
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.RulebookCompendiumImporter
+import kotlinx.coroutines.runBlocking
 import org.junit.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@Ignore("Intended for development only")
 class ImporterTest {
+
     @Test
-    @Ignore("Intended for development only")
-    fun test() {
-        val rulebook = javaClass.getResourceAsStream("rulebook.pdf")
-        PdfReader(rulebook).use { reader ->
+    fun `careers import`() {
+        withRulebook { reader ->
             assertEquals(64, RulebookCareerImporter().importCareers(reader).count())
         }
+    }
+
+    @Test
+    fun `talents import`() {
+        withRulebook { reader ->
+            val talents = runBlocking { RulebookCompendiumImporter(reader).importTalents() }
+
+            assertEquals(167, talents.size)
+        }
+    }
+
+    private fun withRulebook(block: (PdfReader) -> Unit) {
+        val rulebook = javaClass.getResourceAsStream("rulebook.pdf")
+        PdfReader(rulebook).use(block)
     }
 }
