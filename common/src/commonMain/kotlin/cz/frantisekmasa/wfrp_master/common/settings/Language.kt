15f962e1d0f45e39eaa0ec053069ea117155c4de
František Maša <frantisekmasa1@gmail.com>
2023-09-04 17:51:12 +0200

    Sort languages by name

--- common/src/commonMain/kotlin/cz/frantisekmasa/wfrp_master/common/settings/Language.kt
+++ common/src/commonMain/kotlin/cz/frantisekmasa/wfrp_master/common/settings/Language.kt
@@ -2,9 +2,9 @@ package cz.frantisekmasa.wfrp_master.common.settings
 
 enum class Language(
     val localizedName: String,
-    val englishName: String
+    val englishName: String,
 ) {
     BASE("English", "English"),
-    IT("Italiano", "Italian"),
     FR("Français", "French"),
+    IT("Italiano", "Italian"),
 }
