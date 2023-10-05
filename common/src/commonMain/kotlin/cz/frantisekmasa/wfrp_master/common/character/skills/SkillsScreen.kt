da2c7c0ba959724598f62cdd4077f2ce4f7e2e96
František Maša <frantisekmasa1@gmail.com>
2023-10-05 15:47:32 +0200

    Use lazy list for skills, talents and traits

--- common/src/commonMain/kotlin/cz/frantisekmasa/wfrp_master/common/character/skills/SkillsScreen.kt
+++ common/src/commonMain/kotlin/cz/frantisekmasa/wfrp_master/common/character/skills/SkillsScreen.kt
@@ -1,23 +1,28 @@
 package cz.frantisekmasa.wfrp_master.common.character.skills
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import cz.frantisekmasa.wfrp_master.common.character.talents.TalentsCard
-import cz.frantisekmasa.wfrp_master.common.character.traits.TraitsCard
+import cz.frantisekmasa.wfrp_master.common.character.talents.talentsCard
+import cz.frantisekmasa.wfrp_master.common.character.traits.traitsCard
 import cz.frantisekmasa.wfrp_master.common.core.domain.Stats
 import cz.frantisekmasa.wfrp_master.common.core.domain.identifiers.CharacterId
 import cz.frantisekmasa.wfrp_master.common.core.domain.skills.Skill
 import cz.frantisekmasa.wfrp_master.common.core.domain.talents.Talent
 import cz.frantisekmasa.wfrp_master.common.core.domain.traits.Trait
+import cz.frantisekmasa.wfrp_master.common.core.ui.cards.CardContainer
 import cz.frantisekmasa.wfrp_master.common.core.ui.primitives.Spacing
 import cz.frantisekmasa.wfrp_master.common.core.ui.responsive.Breakpoint
-import cz.frantisekmasa.wfrp_master.common.core.ui.responsive.ColumnSize
-import cz.frantisekmasa.wfrp_master.common.core.ui.responsive.Container
+import cz.frantisekmasa.wfrp_master.common.core.ui.responsive.LocalBreakpoint
 import kotlinx.collections.immutable.ImmutableList
 
 @Composable
@@ -29,36 +34,86 @@ fun SkillsScreen(
     removeTalent: (Talent) -> Unit,
     removeTrait: (Trait) -> Unit,
 ) {
-    Container(
-        modifier
-            .background(MaterialTheme.colors.background)
-            .verticalScroll(rememberScrollState())
-            .padding(top = Spacing.small),
-    ) {
-        val size = if (breakpoint > Breakpoint.XSmall) ColumnSize.HalfWidth else ColumnSize.FullWidth
+    if (LocalBreakpoint.current > Breakpoint.XSmall) {
+        Row(
+            modifier
+                .padding(Spacing.small)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(Spacing.medium)
+        ) {
+            CardContainer(Modifier.weight(1f)) {
+                LazyColumn(Modifier.fillMaxWidth()) {
+                    skillsCard(
+                        characterId = characterId,
+                        skills = state.skills,
+                        onRemove = removeSkill,
+                        characteristics = state.characteristics,
+                    )
+                }
+            }
 
-        column(size) {
-            SkillsCard(
+            CardContainer(Modifier.weight(1f)) {
+
+                LazyColumn(Modifier.fillMaxWidth()) {
+                    talentsCard(
+                        characterId = characterId,
+                        talents = state.talents,
+                        onRemove = removeTalent,
+                    )
+
+                    item {
+                        SectionSeparator()
+                    }
+
+                    traitsCard(
+                        characterId = characterId,
+                        traits = state.traits,
+                        onRemove = removeTrait,
+                    )
+                }
+            }
+        }
+    } else {
+        LazyColumn(
+            modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+        ) {
+            skillsCard(
                 characterId = characterId,
                 skills = state.skills,
                 onRemove = removeSkill,
                 characteristics = state.characteristics,
             )
-        }
 
-        column(size) {
-            TalentsCard(
+            item {
+                SectionSeparator()
+            }
+
+            talentsCard(
                 characterId = characterId,
                 talents = state.talents,
                 onRemove = removeTalent,
             )
 
-            TraitsCard(
+            item {
+                SectionSeparator()
+            }
+
+            traitsCard(
                 characterId = characterId,
                 traits = state.traits,
                 onRemove = removeTrait,
             )
         }
+    }
+}
+
+@Composable
+private fun SectionSeparator() {
+    Column {
+        Spacer(Modifier.height(Spacing.medium))
+        Divider()
     }
 }
 
