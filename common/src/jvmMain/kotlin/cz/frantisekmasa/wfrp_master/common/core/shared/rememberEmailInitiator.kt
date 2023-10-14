1b1aa31b09a19d71c0dac2c5396b5fa5b5133e15
František Maša <frantisekmasa1@gmail.com>
2023-10-14 17:04:31 +0200

    Remove dead code

--- common/src/jvmMain/kotlin/cz/frantisekmasa/wfrp_master/common/core/shared/rememberEmailInitiator.kt
+++
@@ -1,9 +0,0 @@
-package cz.frantisekmasa.wfrp_master.common.core.shared
-
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.staticCompositionLocalOf
-
-val LocalEmailInitiator = staticCompositionLocalOf<EmailInitiator> { error("LocalEmailInitiator was not set") }
-
-@Composable
-actual fun rememberEmailInitiator(): EmailInitiator = LocalEmailInitiator.current
