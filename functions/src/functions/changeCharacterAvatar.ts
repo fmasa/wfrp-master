3a2bf7dc172f20f0a8f49d229502e2d87a4a83bd
František Maša <frantisekmasa1@gmail.com>
2024-02-25 23:50:30 +0100

    Copy avatar when duplicating NPC

--- functions/src/functions/changeCharacterAvatar.ts
+++ functions/src/functions/changeCharacterAvatar.ts
@@ -2,9 +2,8 @@ import {storage} from 'firebase-admin';
 import {file} from "tmp-promise";
 import * as sharp from "sharp";
 import * as t from "io-ts";
-import {UploadResponse} from "@google-cloud/storage/build/src/bucket";
-import {Bucket} from "@google-cloud/storage";
 import {characterChange} from "../characterChange";
+import {generateAvatarUrl, getAvatarPath, METADATA} from "../avatar";
 
 const imageSize = 500;
 
@@ -30,15 +29,12 @@ export const changeCharacterAvatar = characterChange(RequestBody,async (body, ch
     const response = await bucket.upload(
         tempFile.path,
         {
-            destination: `images/parties/${partyId}/characters/${characterId}.webp`,
-            metadata: {
-                contentType: "image/webp",
-                cacheControl: `max-age=${365 * 24 * 60 * 60}`,
-            },
+            destination: getAvatarPath(partyId, characterId),
+            metadata: METADATA,
         }
     );
 
-    const url = generateAvatarUrl(response, bucket);
+    const url = await generateAvatarUrl(response, bucket);
 
     console.debug(`File url: ${url}`);
 
@@ -70,18 +66,4 @@ const cropToRectangle = async (image: sharp.Sharp): Promise<sharp.Sharp> => {
         width: size,
         height: size,
     })
-}
-
-const generateAvatarUrl = (response: UploadResponse, bucket: Bucket): string => {
-    if ("FIREBASE_STORAGE_EMULATOR_HOST" in process.env) {
-        return response[1].mediaLink;
-    }
-
-    return "https://firebasestorage.googleapis.com/v0/b/"
-        + bucket.name
-        + "/o/"
-        + encodeURIComponent(response[0].name)
-        + "?alt=media"
-        + "&v="
-        + (+new Date());
 }
--- functions/src/functions/duplicateCharacter.ts
+++ functions/src/functions/duplicateCharacter.ts
@@ -1,7 +1,8 @@
-import {firestore} from 'firebase-admin';
+import {firestore, storage} from 'firebase-admin';
 import * as t from "io-ts";
 import {characterChange} from "../characterChange";
 import { v4 as uuidv4 } from "uuid";
+import {generateAvatarUrl, getAvatarPath, METADATA} from "../avatar";
 
 const RequestBody = t.interface({
     partyId: t.string,
@@ -9,20 +10,26 @@ const RequestBody = t.interface({
     newName: t.string,
 });
 
+type Character = {
+    avatarUrl: string | null;
+}
+
 export const duplicateCharacter = characterChange(RequestBody, async (body, character) => {
     const batch = firestore().batch();
     const newCharacterId = uuidv4();
     const newCharacter = character.parent.doc(newCharacterId);
-    const characterData = (await character.get()).data();
+    const characterData = (await character.get()).data() as Character;
 
-    batch.set(
-        newCharacter,
-        {
-            ...characterData,
-            id: newCharacterId,
-            name: body.newName,
-        }
-    )
+    const newCharacterData =         {
+        ...characterData,
+        id: newCharacterId,
+        name: body.newName,
+        avatarUrl: await copyAvatar(characterData.avatarUrl, body.partyId, body.characterId, newCharacterId),
+    };
+
+    console.log("New character data", newCharacterData);
+
+    batch.set(newCharacter, newCharacterData);
 
     const collections = await character.listCollections();
 
@@ -45,3 +52,34 @@ export const duplicateCharacter = characterChange(RequestBody, async (body, char
         status: "success",
     };
 })
+
+const copyAvatar = async (
+    avatarUrl: string | null,
+    partyId: string,
+    characterId: string,
+    newCharacterId: string,
+): Promise<string | null> => {
+    if (avatarUrl === null) {
+        return null;
+    }
+
+    const bucket = storage().bucket();
+
+    try {
+        const sourceFile = bucket.file(getAvatarPath(partyId, characterId));
+
+        if (!(await sourceFile.exists())[0]) {
+            return null;
+        }
+
+        const copyResponse = await sourceFile.copy(
+            bucket.file(getAvatarPath(partyId, newCharacterId)),
+            {metadata: METADATA}
+        );
+
+        return await generateAvatarUrl(copyResponse, bucket);
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
