eef04ef131f899de56a240eb2e26c02bac2bde0a
František Maša <frantisekmasa1@gmail.com>
2024-06-10 20:24:45 +0200

    Add regression testing and configurable path to test data

---
+++ common/src/jvmTest/kotlin/CompendiumImportRegressionTest.kt
@@ -0,0 +1,141 @@
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.PdfCompendiumImporter
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.ArchivesOfTheEmpire1
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.Book
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.CoreRulebook
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.EnemyInShadowsCompanion
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.UpInArms
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.WindsOfMagic
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.parsers.loadDocument
+import cz.frantisekmasa.wfrp_master.common.compendium.import.BlessingImport
+import cz.frantisekmasa.wfrp_master.common.compendium.import.CareerImport
+import cz.frantisekmasa.wfrp_master.common.compendium.import.MiracleImport
+import cz.frantisekmasa.wfrp_master.common.compendium.import.SkillImport
+import cz.frantisekmasa.wfrp_master.common.compendium.import.SpellImport
+import cz.frantisekmasa.wfrp_master.common.compendium.import.TalentImport
+import cz.frantisekmasa.wfrp_master.common.compendium.import.TraitImport
+import cz.frantisekmasa.wfrp_master.common.compendium.import.TrappingImport
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
+import org.junit.Assume.assumeTrue
+import java.io.File
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.createDirectory
+import kotlin.io.path.createFile
+import kotlin.io.path.writeText
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CompendiumImportRegressionTest {
+    @Test
+    fun `Core Rulebook`() {
+        assertMatchesPreviousRuns(CoreRulebook)
+    }
+
+    @Test
+    fun `Winds of Magic`() {
+        assertMatchesPreviousRuns(WindsOfMagic)
+    }
+
+    @Test
+    fun `Up in Arms`() {
+        assertMatchesPreviousRuns(UpInArms)
+    }
+
+    @Test
+    fun `Enemy in Shadows - Companion`() {
+        assertMatchesPreviousRuns(EnemyInShadowsCompanion)
+    }
+
+    @Test
+    fun `Archives of The Empire I`() {
+        assertMatchesPreviousRuns(ArchivesOfTheEmpire1)
+    }
+
+    private fun assertMatchesPreviousRuns(book: Book) {
+        val pdf = ConfigProvider.getRulebookPdf(book)
+        assumeTrue("PDF is found", pdf != null && pdf.exists())
+        val document = loadDocument(pdf!!.inputStream())
+        val importer = PdfCompendiumImporter(document, book)
+
+        val runner = Runner(bookName = pdf.nameWithoutExtension)
+
+        with(runner) {
+            runBlocking {
+                import("skills", importer.importSkills().map(SkillImport::fromSkill))
+                import("talents", importer.importTalents().map(TalentImport::fromTalent))
+                import("spells", importer.importSpells().map(SpellImport::fromSpell))
+                import("blessings", importer.importBlessings().map(BlessingImport::fromBlessing))
+                import("miracles", importer.importMiracles().map(MiracleImport::fromMiracle))
+                import("careers", importer.importCareers().map(CareerImport::fromCareer))
+                import("traits", importer.importTraits().map(TraitImport::fromTrait))
+                import("trappings", importer.importTrappings().map(TrappingImport::fromTrapping))
+            }
+        }
+
+        assertEquals(emptyList(), runner.failedCompendiums)
+    }
+
+    class Runner(
+        private val bookName: String,
+    ) {
+        val failedCompendiums = mutableListOf<String>()
+
+        inline fun <reified T> import(
+            name: String,
+            items: List<T>,
+        ) {
+            import(name, items, serializer(), serializer())
+        }
+
+        fun <T> import(
+            name: String,
+            items: List<T>,
+            serializationStrategy: SerializationStrategy<List<T>>,
+            deserializationStrategy: DeserializationStrategy<List<T>>,
+        ) {
+            val previousResults =
+                File("${ConfigProvider.getPreviousResultsPath()}/$bookName/$name.json")
+            println("Loading previous $name compendium import result from ${previousResults.absolutePath}")
+            val previousItems =
+                previousResults.takeIf { it.exists() }?.let {
+                    json.decodeFromString(
+                        deserializationStrategy,
+                        it.readText(),
+                    )
+                } ?: emptyList()
+
+            if (items != previousItems) {
+                if (items.isNotEmpty()) {
+                    val outputPath =
+                        Path(ConfigProvider.getOutputPath() ?: error("Output path not set"))
+                    val file =
+                        outputPath.createDirectory()
+                            .resolve(bookName)
+                            .createDirectory()
+                            .resolve("$name.json")
+                            .createFile()
+
+                    println("Writing $name compendium import result to ${file.absolutePathString()}")
+                    val result = json.encodeToString(serializationStrategy, items)
+                    file.writeText(result)
+                } else {
+                    println("No $name compendium import result")
+                }
+                failedCompendiums.add(name)
+            }
+        }
+    }
+
+    companion object {
+        private val json =
+            Json {
+                encodeDefaults = true
+                prettyPrint = true
+            }
+    }
+}
---
+++ common/src/jvmTest/kotlin/ConfigProvider.kt
@@ -0,0 +1,52 @@
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.ArchivesOfTheEmpire1
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.Book
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.CoreRulebook
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.EnemyInShadowsCompanion
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.UpInArms
+import cz.frantisekmasa.wfrp_master.common.compendium.domain.importer.books.WindsOfMagic
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.io.File
+
+object ConfigProvider {
+    fun getRulebookPdf(book: Book): File? {
+        val pdfFileName = bookFileName(book) ?: return null
+        val config = getConfig() ?: return null
+
+        return File(config.rulebooksPath, pdfFileName)
+    }
+
+    fun getPreviousResultsPath(): String? {
+        return getConfig()?.previousResultsPath
+    }
+
+    fun getOutputPath(): String? {
+        return getConfig()?.outputPath
+    }
+
+    private fun bookFileName(book: Book) =
+        when (book) {
+            CoreRulebook -> "rulebook.pdf"
+            WindsOfMagic -> "winds_of_magic.pdf"
+            UpInArms -> "up_in_arms.pdf"
+            EnemyInShadowsCompanion -> "enemy_in_shadows_companion.pdf"
+            ArchivesOfTheEmpire1 -> "archives_of_the_empire_1.pdf"
+            else -> null
+        }
+
+    private fun getConfig(): Config? {
+        val configContent =
+            javaClass.getResourceAsStream("config.json")?.use {
+                it.bufferedReader().readText()
+            } ?: return null
+
+        return Json.decodeFromString<Config>(configContent)
+    }
+}
+
+@Serializable
+data class Config(
+    val previousResultsPath: String,
+    val outputPath: String,
+    val rulebooksPath: String,
+)
